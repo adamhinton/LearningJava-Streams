ae177ff65312185e6b52e28ea51b6657b8b4a482
adamd <adam.d.hinton@gmail.com>
2024-04-03 11:22:34 -0400

    Init commit wed morning

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,38 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<String> bingoPool = new ArrayList<>(75);
+
+        int start = 1;
+        for (char c : "BINGO".toCharArray()){
+            for (int i=start; i< (start + 15); i++){
+                bingoPool.add("" + c + i);
+            }
+            start +=15;
+        }
+
+        Collections.shuffle(bingoPool);
+
+        for (int i=0; i<15; i++){
+            System.out.println(bingoPool.get(i ));
+        }
+        System.out.println("-------------------");
+
+        List<String> firstOnes = bingoPool.subList(0, 15);
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s->{
+            if (s.indexOf('G') == 0 || s.indexOf("O") == 0) {
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.println(updated + " ");
+                return updated;
+            }
+        });
     }
 }
